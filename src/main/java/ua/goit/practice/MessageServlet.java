874da18985534a7d98124e7e70b551e3e2756225
alexyanov <yanov.alexander@gmail.com>
2022-03-31 20:40:58 +0300

    Add get servlet with web.xml mapping

---
+++ src/main/java/ua/goit/practice/MessageServlet.java
@@ -0,0 +1,19 @@
+package ua.goit.practice;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MessageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String text = req.getParameter("text");
+        if (Objects.isNull(text)) {
+            text = "Please write a text in the query parameter";
+        }
+        resp.getWriter().write(text);
+    }
+}
