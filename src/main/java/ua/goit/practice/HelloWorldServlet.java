bbea8834bd845237c9c426070223e707b9576751
alexyanov <yanov.alexander@gmail.com>
2022-03-31 20:57:46 +0300

    Moved to annotations

--- src/main/java/ua/goit/practice/HelloWorldServlet.java
+++ src/main/java/ua/goit/practice/HelloWorldServlet.java
@@ -1,11 +1,13 @@
 package ua.goit.practice;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
--- src/main/java/ua/goit/practice/MessageServlet.java
+++ src/main/java/ua/goit/practice/MessageServlet.java
@@ -1,12 +1,14 @@
 package ua.goit.practice;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 
+@WebServlet(urlPatterns = "/message")
 public class MessageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
